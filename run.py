3ddc3028c53aba50c8398b2d9131f1d1aac611df
razi-aiandyou <razi@aiandyou.io>
2024-11-11 10:07:58 +0700

    Update run.py for webhook-based system and simplify application startup

--- run.py
+++ run.py
@@ -1,28 +1,9 @@
 # run.py
 
-import argparse
 from app import create_app
-import requests
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Run the search and content extraction application.")
-    parser.add_argument("query", help="The search query to use")
-    parser.add_argument("--pages", type=int, default=1, help="Number of pages to crawl (default: 1)")
-    return parser.parse_args()
 
 if __name__ == '__main__':
-    args = parse_arguments()
     app = create_app()
     
-    # Store the arguments in the app config
-    app.config['SEARCH_QUERY'] = args.query
-    app.config['NUM_PAGES'] = args.pages
-    
-    # Start the Flask app in a separate thread
-    from threading import Thread
-    thread = Thread(target=app.run, kwargs={'debug': True, 'use_reloader': False})
-    thread.start()
-
-    # Execute the search request
-    response = requests.get('http://127.0.0.1:5000/search')
-    print(response.json())
+    # Start the Flask app
+    app.run(debug=True, host='0.0.0.0', port=5000)
